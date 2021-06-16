cf7fb2faa8b997311eb6ca1d2e4eed8854f2722c
Pablo Vannini <vanninipablo@gmail.com>
2021-06-16 20:08:03 -0300

    adding spaces to json

--- src/json.cpp
+++ src/json.cpp
@@ -1,19 +1,48 @@
 #include "json.h"
 
+#include <cassert>
+
 #define OBJECT_START '{'
-#define KEY_VALUE_SEPARATOR ':'
+#define OBJECT_MIDDLE ':'
 #define OBJECT_END '}'
 #define STRING_START_END '"'
 
-std::string::iterator parse_string(const std::string raw, const std::string::iterator it_from)
+typedef std::string::iterator str_it;
+
+bool is_whitespace(const str_it &it)
 {
-    for (std::string::iterator it_to = it_from; it_to != raw.end(); it_to++)
-    {
-        if (*it_to == STRING_START_END){
-            return it_to;
-        }
-    }
-    throw "character not found";
+    return *it == ' ';
+}
+
+void consume_whitespace(const std::string raw, str_it &it)
+{
+    for (;is_whitespace(it) && it != raw.end(); it++);
+}
+
+std::string parse_string(const std::string raw, str_it &it)
+{
+    consume_whitespace(raw, it);
+    assert(*it == STRING_START_END);
+    it++;
+    str_it start = it;
+    for (;*it != STRING_START_END && it != raw.end(); it++);
+    assert(it != raw.end());
+    assert(*it == STRING_START_END);
+    return std::string(start, it++);
+}
+
+void consume_object_start(const std::string raw, str_it &it)
+{
+    consume_whitespace(raw, it);
+    assert(*it == OBJECT_START);
+    it++;
+}
+
+void consume_object_middle(const std::string raw, str_it &it)
+{
+    consume_whitespace(raw, it);
+    assert(*it == OBJECT_MIDDLE);
+    it++;
 }
 
 std::string json::operator [](std::string key) const
@@ -23,7 +52,7 @@ std::string json::operator [](std::string key) const
     {
         return it->second;
     }
-    throw "key not found";
+    assert(0); // key not found
 }
 
 std::string & json::operator [](std::string key)
@@ -33,25 +62,19 @@ std::string & json::operator [](std::string key)
     {
         return it->second;
     }
-    throw "key not found";
+    assert(0); // key not found
 }
 
 json json::from_string(std::string raw)
 {
     auto nnn = json();
-    if (*raw.begin() == OBJECT_START)
-    {
-        auto st = raw.begin();
-        st++;
-        st++;
-        auto ed = parse_string(raw, st);
-        auto key = std::string(st, ed);
-        ed++; // avoid :
-        ed++;// avoid "
-        ed++;// start with the string
-        auto eded = parse_string(raw, ed);
-        auto value = std::string(ed, eded);
-        nnn.keyValueMap[key] = value;
-    }
+
+    str_it it = raw.begin();
+    consume_object_start(raw, it);
+    std::string key = parse_string(raw, it);
+    consume_object_middle(raw, it);
+    std::string value = parse_string(raw, it);
+    nnn.keyValueMap[key] = value;
+
     return nnn;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include "json.h"
 
+#include <algorithm>
 #include <string>
 #include <iostream>
+#include <cassert>
+
+inline json process(std::string s)
+{
+    std::replace(s.begin(), s.end(), '\'', '\"');
+    return json::from_string(s);
+}
 
 int main()
 {
-    json test = json::from_string("{\"key\":\"value\"}");
-    if (test["key"] == "value")
-    {
-        std::cout << "OK" << std::endl;
-    }
+    json test;
+
+    // some simple json tests
+    test = process("{'key':'value'}");
+    assert(test["key"] == "value");
+    test = process("{'aka':'value'}");
+    assert(test["aka"] == "value");
+    test = process("{'key':'eee'}");
+    assert(test["key"] == "eee");
+
+    // json with spaces
+    test = process(" { 'key' : 'value' } ");
+    assert(test["key"] == "value");
+    test = process("  {  'key'  :  'value'  }  ");
+    assert(test["key"] == "value");
+    test = process("{          'key'          :     'value'    }     ");
+    assert(test["key"] == "value");
+    test = process(" { '  key  '   : '  value  ' } ");
+    assert(test["  key  "] == "  value  ");
+
     return 0;
 }
