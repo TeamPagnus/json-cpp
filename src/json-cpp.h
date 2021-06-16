360f2ff39b90cc6df486cf45cf2d287a46c5a4a4
Pablo Vannini <vanninipablo@gmail.com>
2021-06-16 13:46:14 -0300

    adding a simple value store

--- src/json-cpp.h
+++ src/json-cpp.h
@@ -1,24 +1,73 @@
 #include <string>
 #include <map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#define OBJECT_START '{'
+#define KEY_VALUE_SEPARATOR ':'
+#define OBJECT_END '}'
+#define STRING_START_END '"'
+
+
+
+std::string::iterator parse_string(const std::string raw, const std::string::iterator it_from)
+{
+    for (std::string::iterator it_to = it_from; it_to != raw.end(); it_to++)
+    {
+        if (*it_to == STRING_START_END){
+            return it_to;
+        }
+    }
+    throw "character not found";
+}
+
+std::vector<std::string> split(std::string teste, char spliter)
+{
+    std::stringstream test(teste);
+    std::string segment;
+    std::vector<std::string> seglist;
+
+    while(std::getline(test, segment, spliter))
+    {
+    seglist.push_back(segment);
+    }
+
+    return seglist;
+}
+
 
 struct json {
-    const std::string tete = "42";
     std::string ttt;
+    std::string second;
 
     std::string operator [](std::string) const
     {
-        return tete;
+        return second;
     }
 
     std::string & operator [](std::string key)
     {
-        return ttt;
+        return second;
     }
 
     static json from_string(std::string raw)
     {
         auto nnn = json();
-        nnn.ttt = "value";
+        if (*raw.begin() == OBJECT_START)
+        {
+            auto st = raw.begin();
+            st++;
+            st++;
+            auto ed = parse_string(raw, st);
+            nnn.ttt = std::string(st, ed);
+            ed++; // avoid :
+            ed++;// avoid "
+            ed++;// start with the string
+            auto eded = parse_string(raw, ed);
+            nnn.second = std::string(ed, eded);
+        }
         return nnn;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,18 @@ int main()
 {
     {
         auto first = json::from_string("{\"key\":\"value\"}");
-        if (first["key"] != "value") {std::cout<<"ERROR"<<std::endl;}
+        if (first["key"] != "value") {std::cout<<"ERROR " << first["key"] << " value"<<std::endl;}
+    }
+    {
+        const auto first = json::from_string("{\"key\":\"value\"}");
+        if (first["key"] != "value") {std::cout<<"ERROR " << first["key"] << " value"<<std::endl;}    }
+    {
+        auto first = json::from_string("{\"key\":\"value3\"}");
+        if (first["key"] != "value3") {std::cout<<"ERROR " << first["key"] << " value3"<<std::endl;}
+    }
+    {
+        const auto first = json::from_string("{\"key\":\"value3\"}");
+        if (first["key"] != "value3") {std::cout<<"ERROR " << first["key"] << " value3"<<std::endl;}
     }
     return 0;
 }
