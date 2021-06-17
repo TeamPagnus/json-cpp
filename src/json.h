d454cd44633ea5a3eb54160a9d8ef45aa63da522
Pablo Vannini <vanninipablo@gmail.com>
2021-06-16 22:43:15 -0300

    adding more tests

--- src/json.h
+++ src/json.h
@@ -14,7 +14,11 @@ class json
 public:
     static json from_string(std::string);
     json operator[](std::string) const;
+    json operator[](size_t) const;
     bool operator==(std::string) const;
+    bool operator==(int) const;
+    bool operator<(json) const;
+    explicit operator bool()const;
 
     std::map<std::string,json> keyValueMap;
     std::string str;
--- src/main.cpp
+++ src/main.cpp
@@ -43,5 +43,73 @@ int main()
     test = process("{'a':{'k':{'u':'a{a}a'}}}");
     assert(test["a"]["k"]["u"] == "a{a}a");
 
+    // object with some key-values
+    test = process("{'key1':'value1','key2':'value2'}");
+    assert(test["key1"] == "value1");
+    assert(test["key2"] == "value2");
+
+    test = process("{'key1':'value1','key2':{'a':'k'}}");
+    assert(test["key1"] == "value1");
+    assert(test["key2"]["a"] == "k");
+
+    test = process("{'a':{'1':'h','2':'e'},'b':{'1':'l','2':'l'},'greet!':'o'}");
+    assert(test["a"]["1"] == "h");
+    assert(test["a"]["2"] == "e");
+    assert(test["b"]["1"] == "l");
+    assert(test["b"]["2"] == "l");
+    assert(test["greet!"] == "o");
+
+    // object with arrays
+    test = process("{'array':['a','o']}");
+    assert(test["array"][0] == "a");
+    assert(test["array"][1] == "o");
+
+    test = process("{'uppercase':['{'a':'A'}}','{'o':'O'},'{'e':'E'}']}");
+    assert(test["uppercase"][0]["a"] == "A");
+    assert(test["uppercase"][1]["o"] == "O");
+    assert(test["uppercase"][2]["e"] == "E");
+
+    // numbers in json!
+
+    test = process("{'number':23}");
+    assert(test["number"] == 23);
+
+    test = process("{'number':-23}");
+    assert(test["number"] == -23);
+
+    test = process("{'number':0}");
+    assert(test["number"] == 0);
+
+    test = process("{'s':0.1,'b':5.1,'n':'-23.2'}");
+    assert(test["s"] < test["b"]);
+    assert(test["n"] < test["s"]);
+    assert(test["n"] < test["b"]);
+
+    test = process("{'a':0.1e10,'b':51e3}");
+    assert(test["a"] < test["b"]);
+
+    // boolean & null values
+
+    test = process("{'a':true}");
+    assert(test["a"]);
+
+    test = process("{'a':false}");
+    assert(!test["a"]);
+
+    test = process("{'e':null}");
+    assert(test["e"] == nullptr);
+
+    // some extra whitespaces
+
+    test = process("\n{\n'key'\n:\n'value'\n}\n");
+    assert(test["key"] == "value");
+    test = process("\t{\t'key'\t:\t'value'\t}\t");
+    assert(test["key"] == "value");
+    test = process("\r{\r'key'\r:\r'value'\r}\r");
+    assert(test["key"] == "value");
+
+    test = process("  \r\n  {  \n   \r\r\r\r\r\t\t\t\t\t\n\n\n\n\n 'e'  :  \t\n 'hello' \n\t   }    \t\t   \t\n");
+    assert(test["e"] == "hello");
+
     return 0;
 }
